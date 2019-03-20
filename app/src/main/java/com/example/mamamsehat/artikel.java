7cf77da8cb7db41f63176a46f6b59f4b2ec6d642
IqbalZunaedi <lyokokyoz@gmail.com>
2019-03-20 21:27:24 +0700

    Update terbaru

---
+++ app/src/main/java/com/example/mamamsehat/artikel.java
@@ -0,0 +1,12 @@
+package com.example.mamamsehat;
+
+import android.os.Bundle;
+
+public class artikel extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artikel);
+    }
+}
