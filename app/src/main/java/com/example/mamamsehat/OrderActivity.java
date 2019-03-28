cca7c5d3f8b1bad683595d40b081216f0bea9a04
Ayu Fitriani <ayufitriany19@gmail.com>
2019-03-28 14:39:44 +0700

    update toolbar

--- app/src/main/java/com/example/mamamsehat/OrderActivity.java
+++ app/src/main/java/com/example/mamamsehat/OrderActivity.java
@@ -1,18 +1,20 @@
 package com.example.mamamsehat;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class OrderActivity extends AppCompatActivity {
-
 
     private RecyclerView mRecyclerView;
     private ArrayList<Produk> mProdukData;
@@ -81,6 +83,47 @@ public class OrderActivity extends AppCompatActivity {
 
         helper.attachToRecyclerView(mRecyclerView);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_beranda:
+                Intent intent = new Intent(OrderActivity.this, Home.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_produk:
+                Intent intent1 = new Intent(OrderActivity.this, OrderActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.action_artikel:
+                Intent intent2 = new Intent(OrderActivity.this, Artikel.class);
+                startActivity(intent2);
+                return true;
+            case R.id.action_forum:
+                Intent intent3 = new Intent(OrderActivity.this, forum.class);
+                startActivity(intent3);
+                return true;
+            case R.id.action_contact:
+                Intent intent4 = new Intent(OrderActivity.this, ContactUs.class);
+                startActivity(intent4);
+                return true;
+            case R.id.action_logout:
+                Intent intent5 = new Intent(OrderActivity.this, Login.class);
+                startActivity(intent5);
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     private void initializeData() {
 
--- app/src/main/java/com/example/mamamsehat/forum.java
+++ app/src/main/java/com/example/mamamsehat/forum.java
@@ -1,7 +1,10 @@
 package com.example.mamamsehat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class forum extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class forum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_beranda:
+                Intent intent = new Intent(forum.this, Home.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_produk:
+                Intent intent1 = new Intent(forum.this, OrderActivity.class);
+                startActivity(intent1);
+                return true;
+            case R.id.action_artikel:
+                Intent intent2 = new Intent(forum.this, Artikel.class);
+                startActivity(intent2);
+                return true;
+            case R.id.action_forum:
+                Intent intent3 = new Intent(forum.this, forum.class);
+                startActivity(intent3);
+                return true;
+            case R.id.action_contact:
+                Intent intent4 = new Intent(forum.this, ContactUs.class);
+                startActivity(intent4);
+                return true;
+            case R.id.action_logout:
+                Intent intent5 = new Intent(forum.this, Login.class);
+                startActivity(intent5);
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
